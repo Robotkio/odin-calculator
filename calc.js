4055770240de9791502dec8bc98bc816063dbf89
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-05-01 22:50:52 -0700

    add operaton function and supporting consts

--- calc.js
+++ calc.js
@@ -1,3 +1,10 @@
+let a, b, operator; // for user input
+const OP_ADD = "+";
+const OP_SUB = "-";
+const OP_MUL = "*";
+const OP_DIV = "/";
+const operators = [OP_ADD, OP_SUB, OP_MUL, OP_DIV];
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +19,15 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operation(a, b, operator) {
+    if (operators.includes(operator)) {
+        switch (operator) {
+            case OP_ADD: return add(a, b);
+            case OP_SUB: return subtract(a, b);
+            case OP_MUL: return multiply(a, b);
+            case OP_DIV: return divide(a, b);
+        }
+    }
 }
